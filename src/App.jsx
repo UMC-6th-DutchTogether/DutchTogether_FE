c585ee9599507a2167172b520021dc27dac5858b
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2024-07-27 02:29:23 +0900

    feat/리덕스 추가 - 프레디
    리덕스 추가 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 import './styles/App.css'
 
+import MainFrom from './screen/MainForm';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SingleLogin from './screen/single/SingleLogin';
+import SingleQ1 from './screen/single/SingleQ1';
+import SingleQ2 from './screen/single/SingleQ2';
+import SingleQ3 from './screen/single/SingleQ3';
+import SingleQ4 from './screen/single/SingleQ4';
+import SingleQ5 from './screen/single/SingleQ5';
+import CheckSingleQ from './screen/single/CheckSingleQ';
+
+
+
+
+
+
 function App() {
-
-
   return (
-    <>
-      Hello World!!!
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainFrom />} />
+        <Route path="/SingleLogin" element={<SingleLogin />} />
+        <Route path="/SingleQ1" element={<SingleQ1 />} />
+        <Route path="/SingleQ2" element={<SingleQ2 />} />
+        <Route path="/SingleQ3" element={<SingleQ3 />} />
+        <Route path="/SingleQ4" element={<SingleQ4 />} />
+        <Route path="/SingleQ5" element={<SingleQ5 />} />
+        <Route path="/CheckSingleQ" element={<CheckSingleQ />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/store/singlePaySlice.jsx
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// 초기 상태 정의
+const initialState = {
+  meetingName: "",
+  bankName: "",
+  accountNumber: "",
+  accountHolder: "",
+  amount: "",
+  numberOfPeople: "",
+};
+
+// createSlice를 사용하여 slice 정의
+const singlePaySlice = createSlice({
+  name: 'singlePay',
+  initialState,
+  reducers: {
+    setMeetingName: (state, action) => {
+      state.meetingName = action.payload;
+    },
+    setBankName: (state, action) => {
+      state.bankName = action.payload;
+    },
+    setAccountNumber: (state, action) => {
+      state.accountNumber = action.payload;
+    },
+    setAccountHolder: (state, action) => {
+      state.accountHolder = action.payload;
+    },
+    setAmount: (state, action) => {
+      state.amount = action.payload;
+    },
+    setNumberOfPeople: (state, action) => {
+      state.numberOfPeople = action.payload;
+    },
+  },
+});
+
+// export
+export const {
+  setMeetingName,
+  setSettlementAmount,
+  setBankName,
+  setAccountNumber,
+  setAccountHolder,
+  setAmount,
+  setNumberOfPeople
+} = singlePaySlice.actions;
+
+export default singlePaySlice.reducer;
