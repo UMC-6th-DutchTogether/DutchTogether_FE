ba332dc8ae9ee61c58f44e85dbaf48a661f3b8cc
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2024-08-17 20:01:20 +0900

    feat/여러명 정산하기 화면 구성

--- src/screen/multi/MultiQ2.jsx
+++ src/screen/multi/MultiQ2.jsx
@@ -20,7 +20,7 @@ export default function MultiQ2() {
   const navigate = useNavigate();
   const { payers, meetingNum } = useSelector((state) => state.multiPay);
   const [inputValue, setInputValue] = useState(payers.map(payer => payer.name).join(',')); // 기존의 payerNames를 기반으로 초기값 설정
-  const [error, setError] = useState(''); // 에러 메시지 상태 
+  //const [error, setError] = useState(''); // 에러 메시지 상태 
 
 
   // 입력시 호출 함수
@@ -34,75 +34,42 @@ export default function MultiQ2() {
     return namesArray.length > 0 && namesArray.every(name => name !== '');
   };
 
-  //링크 get함수
-  const getLink = async () => {
-    console.log(meetingNum)
-    try {
-      const response = await axios.get(`https://umc.dutchtogether.com/api/meetings/${meetingNum}/link`)
-      if (response.status == 200) {
-        console.log(response.data);
-      } else {
-        console.log(response);
-        setError('링크를 받는데 실패했습니다.')
-      }
-    } catch (err) {
-      console.log(response);
-      setError('링크요청 중 오류가 발생했습니다.');
-      console.log(err);
-    }
-  }
+
 
 
 
   // 유효성 검사를 통과하면 다음 페이지로 이동
+
+
   const handleNextClick = async () => {
-    //meetingNum에 대한 링크 받기
-    getLink();
-
     if (isInputValid()) {
-      const namesArray = inputValue.split(',').map(name => name.trim());
-      const newPayers = namesArray.map(name => ({ name, id: " " })); // payer 객체 배열 생성
+      const inputArray = inputValue.split(',').map(name => name.trim());
+      const namesArray = inputArray.map(e => ({ name: e }));//[{name:"이름1"},{name:"이름2"},{name:"이름3"}]
 
+      console.log({
+        payerNames: namesArray,
+        meetingNum: meetingNum
+      });
       try {
         const response = await axios.post('https://umc.dutchtogether.com/api/payers/', {
-          payerNames: newPayers.map(payer => ({ name: payer.name })),
+          payerNames: namesArray,
           meetingNum: meetingNum,
         });
-
-        if (response.status === 200) {
-          console.log(response);
-          const receivedPayers = response.data.data.payers;
-          if (!receivedPayers || receivedPayers.length !== newPayers.length) {
-            setError('API 응답이 payer의 수와 일치하지 않습니다.');
-            return;
-          }
-
-          // 새로 받은 payerId로 newPayers 업데이트
-          const updatedPayers = newPayers.map((payer, index) => {
-            if (receivedPayers[index] && receivedPayers[index].payerId) {
-              return {
-                ...payer,
-                id: receivedPayers[index].payerId
-              };
-            } else {
-              // payerId가 누락된 경우 처리
-              setError(`payerId가 누락되었습니다.`);
-              return payer;
-            }
-          });
-          // Redux에 업데이트된 payers 저장
-          dispatch(setPayers(updatedPayers));
-
-          navigate('/MultiQ3'); // 성공 시 다음 페이지로 이동
-        } else {
-          setError('데이터 전송에 실패했습니다.');
+        if (response.status == 200) {
+          const payersArray = response.data.data.payers// [{payerId:10},{payerId:11},{payerId:12}]
+          const combinedArray = namesArray.map((item, index) =>
+            ({ ...item, ...payersArray[index] })
+          );
+          console.log("결제자 이름 제출", response);
+          dispatch(setPayers(combinedArray));
+          navigate('/MultiQ3');
         }
-      } catch (err) {
-        setError('요청 중 오류가 발생했습니다.');
-        console.error(err);
+      } catch (error) {
+        console.log(error);
       }
-    }
-  };
+
+    };
+  }
 
 
 
---
+++ src/screen/multi/MultiQ6.jsx
@@ -0,0 +1,173 @@
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import {
+  DecorationBarLeft,
+  DecorationBarLeftText,
+  MultiPayContainerLeft,
+  TitleText,
+  NormalText,
+  BigSubmitButton,
+  ContentContainer,
+  TransparentBox,
+  MainBackground,
+  InputListContainer,
+  BorderLine,
+  InputListSmallSection,
+  InputListHeader,
+  InputList,
+  InputListLongHeader,
+  LongInputList,
+  InputListItem,
+  LongInputListItem,
+  DropdownListItem,
+  DropdownList,
+  DropdownIcon,
+  DropdownButton,
+  DropdownContainer,
+  PayerContainer
+} from '../../styles/styledComponents';
+//import {  } from '../../store/multiPaySlice';
+//'https://umc.dutchtogether.com/api/payers/info/{settlerId}' get 정산자 정보 받기
+
+
+export default function MultiQ5() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { payers } = useSelector((state) => state.multiPay);
+
+  // 링크 조회 함수
+  const getLink = async () => {
+    try {
+      const response = axios.get(`https://umc.dutchtogether.com/api/meetings/${meetingNum}/link`)
+      const Link = (await response).data.data.link;
+      console.log('링크', Link);
+      dispatch(setMeetingLink(Link));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  /*
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedPayer, setSelectedPayer] = useState('');
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelectPayer = (payer) => {
+    setSelectedPayer(payer.name); // Store the selected payer's name
+    setIsOpen(false); // Close the dropdown after selection
+  };
+
+  // Local state for input management
+  const [localPayers, setLocalPayers] = useState(payers);
+
+  // Functions to handle input changes
+  const handleBankNameChange = (e, id) => {
+    const updatedPayers = localPayers.map(payer =>
+      payer.id === id ? { ...payer, bankName: e.target.value } : payer
+    );
+    setLocalPayers(updatedPayers);
+  };
+
+  const handleAccountNumberChange = (e, id) => {
+    const updatedPayers = localPayers.map(payer =>
+      payer.id === id ? { ...payer, accountNumber: e.target.value } : payer
+    );
+    setLocalPayers(updatedPayers);
+  };
+
+  const handleSubmit = () => {
+    // Update the Redux state with local changes
+    localPayers.forEach(payer => {
+      dispatch(updatePayer(payer));
+    });
+    console.log('Submitted Data:', localPayers);
+    navigate('/MultiQ4');
+  };
+
+  return (
+    <MainBackground>
+      <DecorationBarLeft>
+        <DecorationBarLeftText>같이 계산해요! </DecorationBarLeftText>
+      </DecorationBarLeft>
+      <TransparentBox />
+
+
+
+
+
+      <MultiPayContainerLeft style={{ height: "1100px", transform: 'translateY(100px)' }}>
+        <TitleText>정산을 진행해주세요!</TitleText>
+        <DropdownContainer>
+          <DropdownButton onClick={toggleDropdown}>
+            {selectedPayer || '당신은 누구입니까?'}
+            <DropdownIcon>{isOpen ? '▲' : '▼'}</DropdownIcon>
+          </DropdownButton>
+          {isOpen && (
+            <DropdownList>
+              {payers.map((payer) => (
+                <DropdownListItem
+                  key={payer.id}
+                  onClick={() => handleSelectPayer(payer)}
+                >
+                  {payer.name}
+                </DropdownListItem>
+              ))}
+            </DropdownList>
+          )}
+        </DropdownContainer>
+        <div style={{ display: 'flex' }}>
+          <TitleText style={{ padding: '0px', margin: "0px 20px 30px" }}>내가 정산해야할 사람은</TitleText>
+          <PayerContainer></PayerContainer>
+        </div>
+
+
+        <ContentContainer>
+          <InputListContainer>
+            <InputListSmallSection>
+              <InputListHeader style={{ marginBottom: '32px' }}>결제자</InputListHeader>
+              <InputList>
+                {localPayers.map((payer, index) => (
+                  <InputListItem key={payer.id}>{payer.name}</InputListItem>
+                ))}
+              </InputList>
+            </InputListSmallSection>
+
+            <BorderLine />
+
+            <InputListSmallSection>
+              <InputListHeader style={{ marginBottom: '32px' }}>금액</InputListHeader>
+              <InputList>
+                {localPayers.map((payer, index) => (
+                  <InputListItem key={payer.id}></InputListItem>
+                ))}
+              </InputList>
+            </InputListSmallSection>
+
+            <BorderLine />
+
+            <div>
+              <InputListLongHeader style={{ marginBottom: '32px', width: '551px' }}>
+                <p style={{ marginLeft: '74px' }}>계좌번호 및 송금하기</p>
+              </InputListLongHeader>
+              <div style={{ display: "flex" }}>
+
+                <LongInputList style={{ marginLeft: '0px' }}>
+                  {localPayers.map((payer, index) => (
+                    <LongInputListItem key={payer.id}>{payer.bankName + " " + payer.accountNumber || ''}</LongInputListItem>
+                  ))}
+
+                </LongInputList>
+              </div>
+            </div>
+
+          </InputListContainer>
+        </ContentContainer>
+      </MultiPayContainerLeft>
+    </MainBackground>
+  );
+  */
+  return;
+}
